df789b5105fb55866848a6ab2b49dc7a8d267a68
ggbtrans <ggbtrans@872ce066-4876-4fca-a6fc-23b94bf576d1>
2016-07-10 19:46:10 +0000

    moodle translation update
    git-svn-id: https://dev.geogebra.org/internal/moodle/moodle-repository_geogebratube/trunk@7913 872ce066-4876-4fca-a6fc-23b94bf576d1

--- lang/zh-CN/repository_geogebratube.php
+++ lang/zh-CN/repository_geogebratube.php
@@ -8,6 +8,6 @@
  * @copyright  (c) International GeoGebra Institute 2014
  * @license        http://www.geogebra.org/license
  */
-$string['configplugin'] = 'GeoGebra库类型配置';
+$string['configplugin'] = 'GeoGebra 库类型配置';
 $string['geogebratube:view'] = '文件选择器中使用 GeoGebra 素材';
 $string['pluginname'] = 'GeoGebra 素材';
